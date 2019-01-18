106858ec904c0a49b51d7366014eb1ce9ea721d8
felloz <serranol82@gmail.com>
2019-01-18 17:05:50 -0400

    Proyecto de carrito de compras terminado

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //Variables
 const carrito = document.getElementById('carrito');
 const cursos  = document.getElementById('lista-cursos');
-const listaCursos = document.querySelector('#lista-carrito', 'tbody'); 
+const listaCursos = document.querySelector('#lista-carrito', 'tbody');
+const vaciarCarritBtn = document.getElementById('vaciar-carrito'); 
 
 
 
@@ -12,7 +13,11 @@ function cargarEventListeners(){
         //Agregar curso al darle click
         cursos.addEventListener('click', comprarCurso);
         //Eliminar Curso
-        carrito.addEventListener('click', eliminarCurso);   
+        carrito.addEventListener('click', eliminarCurso);
+        //Vaciar carrito
+        vaciarCarritBtn.addEventListener('click', vaciarCarrito);
+        //Al cargar el documento mostrar el localstorage
+        document.addEventListener('DOMContentLoaded', leerLocalStorage);   
 
 };
 
@@ -48,14 +53,104 @@ function insertarCarrito(curso){
                      <td><a href="#" class="borrar-curso" data-id="${curso.id}">X</a></td>
     `;
     listaCursos.appendChild(row);
+    guardarCursoLocalStorage(curso);
 }
 
 //Elimina el curso del carrito en el DOM
 function eliminarCurso(e){
     e.preventDefault();
-    let curso;
+    let curso, 
+        cursoId;
+    
     if(e.target.classList.contains('borrar-curso')){
         //hacemos doble parentelement para llegar a la etiqueta que nos interesa eliminar que es la tr
         e.target.parentElement.parentElement.remove();
+        curso = e.target.parentElement.parentElement;
+        cursoId = curso.querySelector('a').getAttribute('data-id');
+        //console.log(cursoId);
     }
+    eliminarCursoLocalStorage(cursoId);
+}
+
+//Funcion que vacia el carrito en el DOM
+
+function vaciarCarrito(e){
+    e.preventDefault();
+    //forma lenta
+    //listaCursos.innerHTML = '';
+    //forma rapida y recomendada
+    while(listaCursos.firstChild){
+        listaCursos.removeChild(listaCursos.firstChild);
+    }
+    
+
+    //vaciar carrito del localstorage
+    vaciarLocalStorage();
+    return false;
+}
+
+//Almacenar cursos en el carrito al local storage
+
+function guardarCursoLocalStorage(curso){
+    let cursos;
+    //Tomar el valor de un arreglo con datos en el LS, lleno o vacio
+    cursos = obtenerCursosLocalStorage();
+
+    cursos.push(curso);
+      localStorage.setItem('cursos', JSON.stringify(cursos) );
+}
+
+//Comprobar que haya elementos en el localstorage
+function obtenerCursosLocalStorage(){
+    let cursosLS;
+
+    //comprobamos si hay informacion en el localstorage
+    if(localStorage.getItem('cursos')=== null){
+        cursosLS = [];
+    }else{
+        cursosLS = JSON.parse(localStorage.getItem('cursos') );
+    }
+    return cursosLS;
+}
+//imprime los cursos de localstorage en el carrito
+function leerLocalStorage(){
+    let cursosLS;
+
+    cursosLS = obtenerCursosLocalStorage();
+    cursosLS.forEach(function(curso){
+        const row = document.createElement('tr');
+    row.innerHTML = `<td><img src="${curso.imagen}" width="100" /></td>
+                     <td>${curso.titulo}</td>
+                     <td>${curso.precio}</td>
+                     <td><a href="#" class="borrar-curso" data-id="${curso.id}">X</a></td>
+    `;
+    listaCursos.appendChild(row);
+    });
+}
+
+
+function eliminarCursoLocalStorage(curso){
+
+    let cursosLS;
+
+    cursosLS = obtenerCursosLocalStorage();
+    //Iteramos los id del curso eliminado con los del localstorage
+    cursosLS. forEach(function(cursoLS, index){
+            if(cursoLS.id === curso){
+                cursosLS.splice(index, 1);
+            }
+    });
+    localStorage.setItem('cursos', JSON.stringify(cursosLS));
+}
+
+function vaciarLocalStorage(){
+    let estavacio;
+        
+     estavacio = obtenerCursosLocalStorage();
+        if(estavacio != ''){
+        confirm('¿Estas seguro que deseas eliminar el carrito?')
+        localStorage.clear();
+        }
+
+    
 }
